bf038f89e84c5691e52bc0e741e44ff85dd4c08a
Atlas <nicolas.droesch@gmail.com>
2022-02-11 15:25:07 +0100

    Dev Activity adding bt in class

---
+++ app/src/main/java/com/example/myexemple_2a/SaisieActivity.java
@@ -0,0 +1,24 @@
+package com.example.myexemple_2a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SaisieActivity extends AppCompatActivity {
+
+    private EditText txtNewTaux;
+    private EditText txtNewDevise;
+    private Object btRetour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie);
+
+        this.txtNewDevise = (EditText) findViewById(R.id.newDevise);
+        this.txtNewTaux = (EditText) findViewById(R.id.newTaux);
+        this.btRetour = (Button) findViewById(R.id.btRetour));
+    }
+}
