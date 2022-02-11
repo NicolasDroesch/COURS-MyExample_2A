c0c09619389f462028e9ab9d0673141db27c1b8f
Atlas <nicolas.droesch@gmail.com>
2022-02-11 15:20:35 +0100

    Nouvelle Activity (saisie)

---
+++ app/src/main/java/com/example/myexemple_2a/Saisie.java
@@ -0,0 +1,14 @@
+package com.example.myexemple_2a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Saisie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie);
+    }
+}
