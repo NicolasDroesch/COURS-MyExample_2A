ebcc48958dba5c7ff2c8d86a3707f1bff39596de
Atlas <nicolas.droesch@gmail.com>
2022-02-11 15:37:01 +0100

    Ajout Intent in Saisie Activity

--- app/src/main/java/com/example/myexemple_2a/SaisieActivity.java
+++ app/src/main/java/com/example/myexemple_2a/SaisieActivity.java
@@ -2,6 +2,7 @@ package com.example.myexemple_2a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,12 @@ public class SaisieActivity extends AppCompatActivity implements View.OnClickLis
             String devise = this.txtNewDevise.getText().toString();
             String taux = this.txtNewTaux.getText().toString();
             //transfert des données à la page principale
+
+            Intent unIntent = new Intent(this, MainActivity.class);
+            unIntent.putExtra("taux", taux);
+            unIntent.putExtra("devise", devise);
+
+            this.startActivity(unIntent);
         }
     }
 }
